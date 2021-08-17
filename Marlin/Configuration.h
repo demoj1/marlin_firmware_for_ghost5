3b9db95f0a2862ea5463e990c2e2c72cc9089adb
Dmitry Dubina <dmitryd.prog@gmail.com>
2021-08-17 11:40:24 +0700

    Поставил TCM2209

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -748,7 +748,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 200, 204 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 200, 804 }
 
 /**
  * Default Max Feed Rate (mm/s)
